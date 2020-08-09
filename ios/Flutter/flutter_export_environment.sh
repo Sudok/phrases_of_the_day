1ba07c147cec6283108045160b95ab5458f04bb3
Sudok <gsr@videvince.com>
2020-08-09 10:20:03 -0300

    Primeiro commit do projeto

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/glauber/Projects/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/glauber/Projects/flutter_projects/frases_do_dia"
+export "FLUTTER_TARGET=/Users/glauber/Projects/flutter_projects/frases_do_dia/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/glauber/Projects/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
